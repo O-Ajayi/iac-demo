b2b9344954c5d4a7d67172fee56b449c64ab64f8
Richie Rich <richierich@Richies-MBP.lan>
2022-01-10 03:11:20 -0500

    updated main config file

--- main.tf
+++ main.tf
@@ -18,17 +18,18 @@ module "app_server" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 3.0"
 
-  name = "single-instance"
+  name = "Jenkins-instance"
 
   ami                    = "ami-0a1eddae0b7f0a79f"
   instance_type          = "t4g.micro"
-  key_name               = "myuser"
+  key_name               = "jenkins-server"
   monitoring             = true
-#   vpc_security_group_ids = ["sg-05faf37a4f06f99e2"]
-  subnet_id              = "subnet-05569c915cff8df56"
+  vpc_security_group_ids = ["sg-0cd8267e17783dfef"]
+  subnet_id              = "subnet-0748037c4eda61cd7"
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
+    Automation = "Jenkins"
   }
 }
